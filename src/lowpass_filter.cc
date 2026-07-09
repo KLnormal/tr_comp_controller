ad5296a8d1a116f745d67b473af3d4769f85b550
tiny_fish <1713828398@qq.com>
2026-07-10 02:27:25 +0800

    chore: 修改马蜂
    大括号不换行；
    增加 @brief。

--- src/lowpass_filter.cc
+++ src/lowpass_filter.cc
@@ -8,7 +8,7 @@
 using namespace filter;
 
 /**
- *
+ * @brief 低通滤波器
  * @param cut_frequency 截止频率（-3dB增益点）
  * @param sample_frec 采样频率
  */
@@ -17,21 +17,18 @@ low_pass_filter::low_pass_filter(float cut_frequency, float sampling_frequency)
 	filter_coe_ = 1/(1+sampling_frequency/(2*PI*cut_frequency));
 }
 
-void low_pass_filter::clear()
-{
+void low_pass_filter::clear() {
 	last_out_ = 0;
 	current_out_ = 0;
 }
 
-void low_pass_filter::set_params(float cut_frequency, float sampling_frequency)
-{
+void low_pass_filter::set_params(float cut_frequency, float sampling_frequency) {
 	cut_frequency_ = cut_frequency;
 	sampling_frequency_ = sampling_frequency;
 	filter_coe_ = 1/(1+sampling_frequency/(2*PI*cut_frequency));
 }
 
-float low_pass_filter::update(float sample)
-{
+float low_pass_filter::update(float sample) {
 	last_out_ = current_out_;
 	current_out_ = (1-filter_coe_)*last_out_ + filter_coe_*sample;
 	return current_out_;
